73544ad84a1bfdd94a18600174681930d831e5c1
Fouche du Preez <fouche@limehouse.co.za>
2023-05-13 12:54:24 +0200

    Added CSRF

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,6 +5,7 @@ import (
 	"github.com/fouched/go-course/pkg/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/justinas/nosurf"
 	"net/http"
 )
 
@@ -13,9 +14,22 @@ func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
 	return mux
+}
+
+func NoSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   false,
+		SameSite: http.SameSiteLaxMode,
+	})
+
+	return csrfHandler
 }
